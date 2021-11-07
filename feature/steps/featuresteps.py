ec694ed5f5057f030733ba57e6ffcf98e863b40a
Jescs <515199703@qq.com>
2021-11-07 23:32:54 +0800

    change

--- feature/steps/featuresteps.py
+++ feature/steps/featuresteps.py
@@ -1,21 +1,24 @@
 from behave import *
+from selenium import webdriver
 
 
 @given(u'launch chrome browser')
 def step_impl(context):
-    assert 1==1
+    driver = webdriver.Chrome()
+    driver.get('https://www.baidu.com')
+    assert 1 == 1
 
 
 @when(u'user get the URL')
 def step_impl(context):
-    assert 3==3
+    assert 3 == 3
 
 
 @then(u'check the logo')
 def step_impl(context):
-    assert 3==3
+    assert 3 == 3
 
 
 @then(u'close')
 def step_impl(context):
-    assert 3==4
+    assert 3 == 4
